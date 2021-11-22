ac00fc7eef43e216c9ac8072b894370a42d3a101
DasciAsli <aslihandasci1@gmail.com>
2021-11-22 18:32:29 +0300

    EntityLayer katmanı olusturuldu

--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,30 @@ namespace EntityLayer.Concrete
     public class Heading //Diğer sınıflardan da erişilebilmesi için erişim belirtecini public yaptık
     {
         //Heading tablosuna ait sütunları ekleyeceğiz(Propertyler)
+        [Key]
         public int HeadingId { get; set; }
+
+
+        [StringLength(50)]
         public string HeadingName { get; set; }
+
+        
         public DateTime HeadingDate { get; set; }
+
+
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+        public ICollection<Content> Contents { get; set; } //Content sınıfı ile ilişkili olacak
+
+
+
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
 
     }
 }
