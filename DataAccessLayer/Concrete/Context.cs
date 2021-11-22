2788c39ce2337a7e9893efcfa3326bff7b07e5c1
DasciAsli <aslihandasci1@gmail.com>
2021-11-22 19:01:50 +0300

    Context sınıfı olusturuldu ve config yapılandırıldı

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context :DbContext //Context sınıfı DbContext sınıfından inherits edildi.
+    {
+
+        public DbSet<About> Abouts { get; set; } //About rojenin içinde yer alan sınıfın adı Abouts ise sqldeki tablonun ismi
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
