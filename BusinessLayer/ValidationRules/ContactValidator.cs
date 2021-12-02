db2c54ba24cb2ab85b412d69af46c456266b968f
DasciAsli <aslihandasci1@gmail.com>
2021-12-02 14:33:39 +0300

    Mesaj Detay kısmı yapıldı

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ContactValidator:AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("Mail Adresini Boş Geçemezsiniz");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu Adını Boş Bırakamazsınız");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı Adını Boş Bırakamazsınız");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapın");
+            RuleFor(x => x.Subject).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayınız");
+        }
+    }
+}
