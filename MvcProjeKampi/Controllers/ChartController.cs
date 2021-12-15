b07ef04fa79701e585dae3510e5e786e1e1c2b17
DasciAsli <aslihandasci1@gmail.com>
2021-12-15 16:21:54 +0300

    Chart olusturuldu

---
+++ MvcProjeKampi/Controllers/ChartController.cs
@@ -0,0 +1,56 @@
+﻿using EntityLayer.Concrete;
+using MvcProjeKampi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    [AllowAnonymous]
+    public class ChartController : Controller
+    {
+
+        //Oluşturdugumuz chartı burada sayfaya göndereceğiz
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Char işlemlerini yaptık
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(),JsonRequestBehavior.AllowGet);
+        }
+
+
+        //BlogList bir metod içini doldurduk sınıfımızın
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct = new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName="Yazılım",
+                CategoryCount=8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Seyehat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+            return ct;
+        }
+    }
+}
